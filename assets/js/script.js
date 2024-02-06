ccf16507bac82ed99ac84a87d446f20de85818ad
Tariq Ebden <tebden16@gmail.com>
2024-02-06 16:17:18 +0000

    added code to disable next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,55 @@
 // --------------------- Question Array
 const countryQuestions = [{
-  question: 'Which country is known as the Land of the Rising Sun?',
-  answers: ['Nepal', 'New Zealand', 'Japan', 'Kazakhstan'],
-  correctAnswer: 'Japan'
-},
-{
-  question: 'What is the official language of Brazil?',
-  answers: ['Spanish', 'English', 'Italian', 'Portuguese'],
-  correctAnswer: 'Portuguese'
-},
-{
-  question: 'Which European country is famous for its windmills, tulips, and canals?',
-  answers: ['Austria', 'Netherlands', 'Slovakia', 'Switzerland'],
-  correctAnswer: 'Netherlands'
-},
-{
-  question: 'In which country would you find the ancient city of Petra?',
-  answers: ['Qatar', 'Jordan', 'Turkey', 'Saudi Arabia'],
-  correctAnswer: 'Jordan'
-},
-{
-  question: 'What is the capital city of Canada?',
-  answers: ['Ottawa', 'Vancouver', 'Toronto', 'Québec City'],
-  correctAnswer: 'Ottawa'
-},
-{
-  question: 'What is the largest island country in the world by land area?',
-  answers: ['Japan', 'Madagascar', 'Indonesia', 'Philippines'],
-  correctAnswer: 'Indonesia'
-},
-{
-  question: 'Which Asian country is the world\'s most populous?',
-  answers: ['China', 'Indonesia', 'Pakistan', 'India'],
-  correctAnswer: 'India'
-},
-{
-  question: 'Which country is home to the Great Barrier Reef, one of the world\'s most famous natural wonders?',
-  answers: ['New Zealand', 'Australia', 'Indonesia', 'Japan'],
-  correctAnswer: 'Australia'
-},
-{
-  question: 'What is the smallest country in the world by land area?',
-  answers: ['Luxembourg', 'Liechtenstein', 'Vatican City', 'Singapore'],
-  correctAnswer: 'Vatican City'
-},
-{
-  question: 'In which country would you find the ancient city of Machu Picchu?',
-  answers: ['Peru', 'Brazil', 'Colombia', 'Panama'],
-  correctAnswer: 'Peru'
-}];
+    question: 'Which country is known as the Land of the Rising Sun?',
+    answers: ['Nepal', 'New Zealand', 'Japan', 'Kazakhstan'],
+    correctAnswer: 'Japan'
+  },
+  {
+    question: 'What is the official language of Brazil?',
+    answers: ['Spanish', 'English', 'Italian', 'Portuguese'],
+    correctAnswer: 'Portuguese'
+  },
+  {
+    question: 'Which European country is famous for its windmills, tulips, and canals?',
+    answers: ['Austria', 'Netherlands', 'Slovakia', 'Switzerland'],
+    correctAnswer: 'Netherlands'
+  },
+  {
+    question: 'In which country would you find the ancient city of Petra?',
+    answers: ['Qatar', 'Jordan', 'Turkey', 'Saudi Arabia'],
+    correctAnswer: 'Jordan'
+  },
+  {
+    question: 'What is the capital city of Canada?',
+    answers: ['Ottawa', 'Vancouver', 'Toronto', 'Québec City'],
+    correctAnswer: 'Ottawa'
+  },
+  {
+    question: 'What is the largest island country in the world by land area?',
+    answers: ['Japan', 'Madagascar', 'Indonesia', 'Philippines'],
+    correctAnswer: 'Indonesia'
+  },
+  {
+    question: 'Which Asian country is the world\'s most populous?',
+    answers: ['China', 'Indonesia', 'Pakistan', 'India'],
+    correctAnswer: 'India'
+  },
+  {
+    question: 'Which country is home to the Great Barrier Reef, one of the world\'s most famous natural wonders?',
+    answers: ['New Zealand', 'Australia', 'Indonesia', 'Japan'],
+    correctAnswer: 'Australia'
+  },
+  {
+    question: 'What is the smallest country in the world by land area?',
+    answers: ['Luxembourg', 'Liechtenstein', 'Vatican City', 'Singapore'],
+    correctAnswer: 'Vatican City'
+  },
+  {
+    question: 'In which country would you find the ancient city of Machu Picchu?',
+    answers: ['Peru', 'Brazil', 'Colombia', 'Panama'],
+    correctAnswer: 'Peru'
+  }
+];
 
 // --------------- Assigning elements to variables
 const questionElem = document.getElementById('quiz-question');
@@ -93,14 +94,6 @@ nextBtn.addEventListener("click", (displayNext = () => {
 function nextQuestion() {
   currentQuestionNumber++;
   document.getElementById('question').textContent = `Question ${currentQuestionNumber} of 10`;
-}
-
-// increment current score in case of correct answer
-let score = 0;
-
-function incrementScore() {
-  let oldScore = parseInt(document.getElementById("score").innerText);
-  document.getElementById("score").innerText = ++oldScore;
 
   /**
    * disabled next button once it reaches last question
@@ -112,7 +105,14 @@ function incrementScore() {
     alert(`You have completed the quiz, you got ${score} out of 10!`)
   }
   displayQuestions(questionIdx);
+}
 
+// increment current score in case of correct answer
+let score = 0;
+
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
 }
 
 //increment current score in case of incorrect answer
